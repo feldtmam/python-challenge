a732284fb2953a28c9c85846a3b8081ae715884a
CodeByMF <ourcreatefactory@gmail.com>
2019-09-08 11:20:13 -0400

    PyBank Stats completed

--- PyBank/main.py
+++ PyBank/main.py
@@ -13,24 +13,76 @@ with open(csvpath, "r" ) as my_file_handle:
     print(f"CSV Header: {csv_header}")
 
     # Read each row of data after the header
-    for row in csvreader:
-        print(row)
-#except IOError:
-    #print("File not found or path is incorrect")
-#finally:
-    #print("exit")
+    my_unique_months = set()
+    values = []
+    all_months = []
+    net_total_amount = 0
+    for rows in csvreader:
+        # identify the number of months by reading the values into a set for unique values 
+        my_unique_months.add(rows[0])
+        all_months.append(rows[0])
+        values.append(rows[1])
+        # Calculate the net total amount of profit/loss over the entire period
+        net_total_amount += int(rows[1])
+    total_months = len(my_unique_months)
 
-# Identify the total number of months
+    # Calculate the average of the changes in profit/loss over the period   
+    values_change = [int(values[i + 1]) - int(values[i]) for i in range(len(values)- 1)]
+    sum_changed_values = 0
+    for i in values_change:
+        sum_changed_values += int(i)
+    average_change = sum_changed_values / len(values_change)
 
-# Calculate the net total amount of profit/loss over the entire period
+    # Identify the greatest increase in profits (date and amount) over the entire period
+    highest_profit_value = max(values_change)
+    highest_profit_value_index = values_change.index(highest_profit_value)
+    highest_profit_month = all_months[highest_profit_value_index + 1]
+    
+    # The greatest decrease in losses (date and amount) over the entire period
+    lowest_profit_value = min(values_change)
+    lowest_profit_value_index = values_change.index(lowest_profit_value)
+    lowest_profit_month = all_months[lowest_profit_value_index + 1]    
+    
+    # Print results to the terminal
+    print("Financial Analysis")
+    print("--------------------------------")
+    print("Total months: ", total_months)
+    print("Total: $", net_total_amount)
+    print("Average Change: $", round(average_change, 2))
+    print("Greatest Increase in Profits:", highest_profit_month, "($",highest_profit_value,")")
+    print("Greatest Decrease in Profits:", lowest_profit_month, "($",lowest_profit_value,")")
 
-# Calculate the average of the changes in profit/loss over the period
+# write the results to a file
+output_path = os.path.join("/Users/feldtmam1/Documents/Data Analytics Bootcamp/repos/python-challenge/PyBank/", "results.csv")
+# Open the file using "write" mode. Specify the variable to hold the contents
+with open(output_path, 'w', newline='') as resultsfile:
 
-# Identify the greatest increase in profits (date and amount) over the entire period
+    # Initialize csv.writer
+    csvwriter = csv.writer(resultsfile, quoting=csv.QUOTE_NONE)
+    header1 = "Financial Analysis"
+    header2 = "--------------------------------"
+    total_write = "Total months: ", total_months
 
-# The greatest decrease in losses (date and amount) over the entire period
+    # Write the content
+    csvwriter.writerow(["Financial Analysis"])
+    csvwriter.writerow(["--------------------------------"])
+    csvwriter.writerow(total_write)
+    csvwriter.writerow(["Total: $", net_total_amount])
+    csvwriter.writerow(["Average Change: $", round(average_change, 2)])
+    csvwriter.writerow(["Greatest Increase in Profits:", highest_profit_month, "($",highest_profit_value,")"])
+    csvwriter.writerow(["Greatest Decrease in Profits:", lowest_profit_month, "($",lowest_profit_value,")"])
 
-# Print results to the terminal
+    
+
+
+
+
+
+
+
+
+
+
 # Financial Analysis
 # ----------------------------
 # Total Months: 86
